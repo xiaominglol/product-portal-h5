fd5561467c43b2f4f9ea056b8b53053953ab80b6
xiaominglol <474345633@qq.com>
2019-12-09 18:05:35 +0800

    初始化

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,32 @@
 <template>
-  <div class="hello">
-    <van-tabbar v-model="active">
-      <van-tabbar-item icon="home-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="search">标签</van-tabbar-item>
-      <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
-      <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
-    </van-tabbar>
-  </div>
+    <div class="hello">
+        <van-tabbar v-model="active" @change="onChange">
+            <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+            <van-tabbar-item icon="search">分类</van-tabbar-item>
+            <van-tabbar-item icon="setting-o">购物车</van-tabbar-item>
+            <van-tabbar-item icon="friends-o">我的</van-tabbar-item>
+        </van-tabbar>
+    </div>
 </template>
 
 <script>
-  import { Tabbar } from 'vant';
-  import { TabbarItem } from 'vant';
-  export default {
-    data() {
-      return {
-        active: 0
-      }
+    import {Tabbar, TabbarItem} from 'vant';
+
+    export default {
+        data() {
+            return {
+                active: 0
+            }
+        },
+        components: {
+            [Tabbar.name]: Tabbar,
+            [TabbarItem.name]: TabbarItem
+        },
+        methods: {
+            onChange(index) {
+                // Notify({ type: 'primary', message: index });
+                alert(index);
+            }
+        }
     }
-  }
 </script>
