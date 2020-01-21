8b4f0960f998f22c211f1c0e58c7f9617baa2a83
xiaominglol <474345633@qq.com>
2020-01-21 18:05:42 +0800

    默认更改列表

--- src/views/goods/GoodsDetail.vue
+++ src/views/goods/GoodsDetail.vue
@@ -98,10 +98,8 @@
                 :properties="properties"
                 :goods="goods"
                 :goods-id="goodsId"
-                :quota="quota"
-                :quota-used="quotaUsed"
-                :hide-stock="sku.hide_stock"
-                :message-config="messageConfig"
+                hide-quota-text
+                close-on-click-overlay
                 @buy-clicked="onBuyClicked"
                 @add-cart="onAddCartClicked"
         />
@@ -160,13 +158,8 @@
                 ],
                 // sku参数开始
                 show: false,
-                quotaUsed: 0,
                 goodsId: 0,
-                quota: 0,
                 sku: {
-                    // 数据结构见下方文档
-                    // 所有sku规格类目与其值的从属关系，比如商品有颜色和尺码两大类规格，颜色下面又有红色和蓝色两个规格值。
-                    // 可以理解为一个商品可以有多个规格类目，一个规格类目下可以有多个规格值。
                     tree: [
                         {
                             k: '颜色', // skuKeyName：规格类目名称
@@ -184,18 +177,36 @@
                                     previewImgUrl: 'https://img.yzcdn.cn/2p.jpg',
                                 }
                             ],
-                            k_s: 's1' // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
+                            k_s: 's11' // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
+                        },
+                        {
+                            k: '规格', // skuKeyName：规格类目名称
+                            v: [
+                                {
+                                    id: '1', // skuValueId：规格值 id
+                                    name: '红色', // skuValueName：规格值名称
+                                    imgUrl: 'https://img.yzcdn.cn/1.jpg', // 规格类目图片，只有第一个规格类目可以定义图片
+                                    previewImgUrl: 'https://img.yzcdn.cn/1p.jpg', // 用于预览显示的规格类目图片
+                                },
+                                {
+                                    id: '2',
+                                    name: '蓝色',
+                                    imgUrl: 'https://img.yzcdn.cn/2.jpg',
+                                    previewImgUrl: 'https://img.yzcdn.cn/2p.jpg',
+                                }
+                            ],
+                            k_s: 's2' // skuKeyStr：sku 组合列表（下方 list）中当前类目对应的 key 值，value 值会是从属于当前类目的一个规格值 id
                         }
                     ],
                     // 所有 sku 的组合列表，比如红色、M 码为一个 sku 组合，红色、S 码为另一个组合
                     list: [
                         {
                             id: 2259, // skuId，下单时后端需要
-                            price: 100, // 价格（单位分）
-                            s1: '1215', // 规格类目 k_s 为 s1 的对应规格值 id
-                            s2: '1193', // 规格类目 k_s 为 s2 的对应规格值 id
+                            price: 120, // 价格（单位分）
+                            s11: '1215', // 规格类目 k_s 为 s1 的对应规格值 id
+                            s2: '1', // 规格类目 k_s 为 s2 的对应规格值 id
                             s3: '0', // 最多包含3个规格值，为0表示不存在该规格
-                            stock_num: 110 // 当前 sku 组合对应的库存
+                            stock_num: 40 // 当前 sku 组合对应的库存
                         }
                     ],
                     price: '1.00', // 默认价格（单位元）
@@ -206,14 +217,13 @@
                         {
                             // 商品留言
                             datetime: '0', // 留言类型为 time 时，是否含日期。'1' 表示包含
-                            multiple: '0', // 留言类型为 text 时，是否多行文本。'1' 表示多行
+                            multiple: '1', // 留言类型为 text 时，是否多行文本。'1' 表示多行
                             name: '留言', // 留言名称
                             type: 'text', // 留言类型，可选: id_no（身份证）, text, tel, date, time, email
-                            required: '1', // 是否必填 '1' 表示必填
-                            placeholder: '' // 可选值，占位文本
+                            // required: '1', // 是否必填 '1' 表示必填
+                            placeholder: '可填留言' // 可选值，占位文本
                         }
                     ],
-                    hide_stock: false // 是否隐藏剩余库存
                 },
                 properties: [ // 商品属性
                     {
@@ -235,10 +245,10 @@
                     }
                 ],
                 goods: {
-                    // 数据结构见下方文档
-                },
-                messageConfig: {
-                    // 数据结构见下方文档
+                    // 商品标题
+                    title: '测试商品',
+                    // 默认商品 sku 缩略图
+                    picture: 'https://img.yzcdn.cn/1.jpg'
                 }
                 // sku参数结束
             };
